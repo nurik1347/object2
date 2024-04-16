2aa2ba2a4d5484053be8fc7292ba9841465fc054
nurik1347 <nurikmirzaev1347@gmail.com>
2024-04-16 16:37:31 +0500

    done

---
+++ main.js
@@ -0,0 +1,10 @@
+let p = prompt('Ism kiriting')
+let m = prompt('Space idingizni kiriting')
+let m1 = prompt('Space password')
+
+let obj ={
+    Ism:p,
+    Id:m,
+    Password:m1,
+}
+console.log(obj);
